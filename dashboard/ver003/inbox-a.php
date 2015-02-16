b15545286bb01958540d980ec8f6b3e7c8b51b2a
danielle fortier <dtfortier@gmail.com>
2015-02-16 16:35:23 -0500

    new

---
+++ dashboard/ver003/inbox-a.php
@@ -0,0 +1,64 @@
+
+<table class="table">
+        
+    <h4 class="admin"><i class="fa fa-suitcase fa-fw"></i> Admin Inbox</h4>
+
+    <thead class="no-border">
+        <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Created</th>
+                <th>Last Login</th>
+           
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="select">
+            
+            <td>Joe Schmoe</td>
+            <td>JoeS@aol.com</td>
+            <td>Jul 1, 2009 12:00 AM</td>
+            <td>Aug 25, 2014 3:36 PM</td>
+        </tr>
+        <tr class="select">
+            
+             <td>Joe Schmoe</td>
+            <td>JoeS@aol.com</td>
+            <td>Jul 1, 2009 12:00 AM</td>
+            <td>Aug 25, 2014 3:36 PM</td>
+        </tr>
+        <tr class="select">
+            
+             <td>Joe Schmoe</td>
+            <td>JoeS@aol.com</td>
+            <td>Jul 1, 2009 12:00 AM</td>
+            <td>Aug 25, 2014 3:36 PM</td>
+        </tr>
+        <tr class="select">
+            
+             <td>Joe Schmoe</td>
+            <td>JoeS@aol.com</td>
+            <td>Jul 1, 2009 12:00 AM</td>
+            <td>Aug 25, 2014 3:36 PM</td>
+        </tr>
+        <tr class="select">
+             <td>Joe Schmoe</td>
+            <td>JoeS@aol.com</td>
+            <td>Jul 1, 2009 12:00 AM</td>
+            <td>Aug 25, 2014 3:36 PM</td>
+        </tr>
+        
+    </tbody>
+</table>
+
+
+    <ul class="pagination">
+      <li><a href="#">&laquo;</a></li>
+      <li><a href="#">1</a></li>
+      <li><a href="#">2</a></li>
+      <li><a href="#">3</a></li>
+      <li><a href="#">4</a></li>
+      <li><a href="#">5</a></li>
+      <li><a href="#">&raquo;</a></li>
+    </ul>
+</div
---
+++ dashboard/ver003/modal.php
@@ -0,0 +1,189 @@
+
+<!-- Modal Change Accounts-->
+<div id="modal-accounts">
+<div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title" id="myModalLabel"><i class="fa fa-suitcase fa-fw"></i> My Accounts</h4>
+          </div>
+          <div class="modal-body">
+            <div class="panel panel-default">
+            <!-- Default panel contents -->
+            <!--<div class="panel-heading">Panel heading</div>-->
+
+            <!-- Table -->
+        
+<table class="table">
+    <thead class="no-border">
+        <tr>
+                <th>LE Account</th>
+                <th>Account Number</th>
+                <th>Address</th>
+                <th>Phone Number</th>
+           
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="select">
+            
+            <td>Aesthetic Center for Plastic Surgery</td>
+            <td>324517</td>
+            <td>12727 Kimberley Suite 300 Houston, TX</td>
+            <td>(281) 407-0281</td>
+        </tr>
+        <tr class="select">
+            
+            <td>Aesthetic Center for Plastic Surgery - Dr. Fortes</td>
+            <td>324517</td>
+            <td>12727 Kimberley Suite 300 Houston, TX</td>
+            <td>(713) 799-9999</td>
+        </tr>
+        <tr class="select">
+            
+            <td>Aesthetic Center for Plastic Surgery - Dr. Morales</td>
+            <td>324517</td>
+            <td>12727 Kimberley Suite 300 Houston, TX</td>
+            <td>(281) 407-0281</td>
+        </tr>
+        <tr class="select">
+            
+            <td>Aesthetic Center for Plastic Surgery - Dr. Morales</td>
+            <td>324517</td>
+            <td>12727 Kimberley Suite 300 Houston, TX</td>
+            <td>(281) 407-0281</td>
+        </tr>
+        <tr class="select">
+            
+            <td>Aesthetic Center for Plastic Surgery - Dr. Morales</td>
+            <td>324517</td>
+            <td>12727 Kimberley Suite 300 Houston, TX</td>
+            <td>(281) 407-0281</td>
+        </tr>
+    </tbody>
+</table>
+          </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-modal-close" data-dismiss="modal">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+</div> <!-- end modal-accounts -->
+
+
+<!-- Modal Support Link -->
+<div id="modal-support">
+    <div class="modal fade" id="supportModal" tabindex="-1" role="dialog" aria-labelledby="supportModal" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+            <h4 class="modal-title" id="myModalLabel"><i class="fa fa-comments fa-lg dark-gray"></i> Support</h4>
+          </div>
+          <div class="modal-body">
+              <h4>Digital Customer Care</h4>
+              <p><i class="fa fa-phone fa-lg dark-gray"></i> 1-866-872-9327</p>
+              <p><i class="fa fa-envelope fa-lg dark-gray"></i> ccare@localedge.com</p>
+          </div>
+          <div class="modal-footer">
+              <div class="left"><a href="#"><i class="fa fa-question-circle fa-lg dark-gray"></i> Help</a></div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+           
+<!-- Modal Quick Post -->
+<div class="modal fade" id="postSuccess" tabindex="-1" role="dialog" aria-labelledby="postSuccess" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Success!</h4>
+      </div>
+      <div class="modal-body">
+       You have successfully posted to Facebook, Twitter, and your blog.
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<!-- Modal "Wanna Come Back?" -->
+
+<div class="modal fade" id="comeBack" tabindex="-1" role="dialog" aria-labelledby="comeBack" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel"><i class="fa fa-check-circle"></i> Display Advertising</h4>
+      </div>
+      <div class="modal-body">
+          <div class="alert alert-upsell" role="alert"><i class="fa fa-quote-left fa-3x pull-left"></i>
+              <p>Target potential customers based on specific behaviors and segments. Drive more qualified leads to your business.</p></div>
+        <form>
+        <div class="form-group">
+           
+            <input type="text" class="form-control" id="inputFName" placeholder="First Name">
+        </div>
+        <div class="form-group">
+            
+            <input type="text" class="form-control" id="inputLName" placeholder="Last Name">
+        </div>
+        <div class="form-group">
+            
+            <input type="tel" class="form-control" id="inputPhone" placeholder="Phone Number">
+        </div>
+        <div class="form-group">
+           
+            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+        </div>
+        <div class="form-group">
+            
+            <textarea class="form-control" id="inputComments" placeholder="Comments" rows="3"></textarea>
+        </div>
+
+    </form>
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-upsell">Send</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal RSS link -->
+<div class="modal fade" id="RSS" tabindex="-1" role="dialog" aria-labelledby="RSS" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">RSS</h4>
+      </div>
+      <div class="modal-body">
+          <p>Your Blog's RSS Feed Link: <a href="#">http://www.LocalEdge.com/rssblog</a></br></br>
+          
+RSS Feeds are web formats that show recently updated content.
+The above RSS Feed is from the Blog on your Landing Page. Anytime you make a post in this Customer Dashboard to your Blog, that post will display on your Blog and in this RSS Feed.
+</br>
+In addition to your Blog displaying on your Landing Page, you can use this RSS Feed to put your Blog on your Website.
+</br></br>
+1. Copy the link above</br>
+
+2. Open your Website management portal</br>
+
+3. Create a Blog or RSS page within your Website</br>
+
+4. Paste the RSS feed into the appropriate RSS section and save your changes</br>
+
+</br>
+If you need assistance, please contact your webmaster.</p>
+      </div>
+      
+    </div>
+  </div>
+</div>
